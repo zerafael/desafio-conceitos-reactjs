7411b689905e4455e6a4ec05b41a38693f71b0f6
José Rafael <zerafael1@gmail.com>
2020-05-22 00:33:56 -0300

    Implement solution

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+	const [repositories, setRepositories] = useState([]);
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+	useEffect(() => {
+		api.get("repositories").then((response) => {
+			setRepositories(response.data);
+		});
+	}, []);
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+	async function handleAddRepository() {
+		const response = await api.post("repositories", {
+			title: `Novo repositório ${Date.now()}`,
+			url: `github.com/${Date.now()}`,
+			techs: ["Javascript", "React", "React Native"],
+		});
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+		setRepositories([...repositories, response.data]);
+	}
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+	async function handleRemoveRepository(id) {
+		const response = await api.delete(`repositories/${id}`);
+
+		if (response.status === 204) {
+			const repositoryIndex = repositories.findIndex(
+				(repository) => repository.id === id
+			);
+
+			const repositoriesTmp = [...repositories];
+			repositoriesTmp.splice(repositoryIndex, 1);
+			setRepositories(repositoriesTmp);
+		}
+	}
+
+	return (
+		<div>
+			<ul data-testid="repository-list">
+				{repositories.map((repository) => (
+					<li key={repository.id}>
+						{repository.title}
+						<button onClick={() => handleRemoveRepository(repository.id)}>
+							Remover
+						</button>
+					</li>
+				))}
+			</ul>
+
+			<button onClick={handleAddRepository}>Adicionar</button>
+		</div>
+	);
 }
 
 export default App;
